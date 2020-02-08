ba99d380a8a29f21b3a60e97dd9c8266d660179e
Hubert Siwkin <hsiwkin@gmail.com>
2020-02-08 18:23:38 +0100

    Initial commit
    Signed-off-by: Hubert Siwkin <hsiwkin@gmail.com>

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("yo I'm a Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(horizontalMovement) > 0)
+        {
+            Debug.Log("horizontal: " + horizontalMovement);
+        }
+
+        if (Mathf.Abs(verticalMovement) > 0)
+        {
+            Debug.Log("vertical: " + verticalMovement);
+        }
+    }
+}
